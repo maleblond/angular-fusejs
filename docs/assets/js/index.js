ff982ee2056bd631793fa1bc6cc21341b0c873f2
Marc-Antoine Leblond <maleblond@petalmd.com>
2017-01-30 12:13:04 -0500

    Add some spacing for PullRequest

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,5 +1,7 @@
 global.angular = require('angular');
 
+
+
 // You need to "npm link angular-local-search", as it is not in package.json.
 // Thus, I do not need to publish to npm registry to test a new feature.
 angular.module('main', [require('angular-local-search')])
